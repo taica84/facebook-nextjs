5721018439a9a8b6b04bc650c3f4a1ede488d432
taica84 <taica_rules@yahoo.com>
2021-06-22 22:11:09 +0300

    add widgets

--- components/feed/InputBox.js
+++ components/feed/InputBox.js
@@ -1,9 +1,10 @@
+
 import {useSession } from "next-auth/client";
 import Image from 'next/image'; 
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon,VideoCameraIcon } from "@heroicons/react/solid";
-import {useRef} from 'react';
-import { db } from "../../firebase";
+import {useRef , useState} from 'react';
+import { db , storage } from "../../firebase";
 import firebase from 'firebase';
 
 
@@ -11,6 +12,8 @@ const InputBox = () => {
     const [session] = useSession();
     const inputRef = useRef(null);
     const filepickerRef = useRef(null);
+
+    const [imageToPost , setImageToPost] = useState(null)
 
     const sendPost = (e) => {
         e.preventDefault();
@@ -22,7 +25,24 @@ const InputBox = () => {
                 email:session.user.email,
                 image:session.user.image,
                 timestamp:firebase.firestore.FieldValue.serverTimestamp()
+            }).then((doc) => {
+                if(imageToPost) {
+                    const uploadTask = storage.ref(`posts/${doc.id}`).putString(imageToPost,'data_url');
+
+                    removeImageFromPost();
+
+                    uploadTask.on("state_change" , null, (error) => console.error(error) , () => {
+                        storage.ref('posts').child(doc.id).getDownloadURL().then((url) => {
+                            db.collection('posts').doc(doc.id).set({
+                                postImage:url
+                            },{merge:true})
+                        })
+                    })
+                   
+                }
             })
+
+
             inputRef.current.value = ""
         }else {
             return
@@ -30,8 +50,21 @@ const InputBox = () => {
     };
 
     const addImageToPost = (e) => {
-        
+        const reader = new FileReader();
+        if(e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+        reader.onload = (readerEvent) => {
+             setImageToPost(readerEvent.target.result)
+        }
+    };
+
+    const removeImageFromPost = () => {
+        setImageToPost(null)
     }
+
+
+
 
     return (
         <div className='bg-white p-2 rounded-2xl shadow-md text-gray-500 mt-6 font-medium' >
@@ -57,14 +90,32 @@ const InputBox = () => {
                       Submit
                   </button>
               </form>
+               
+              {imageToPost && (
+                   <div onClick={removeImageFromPost} className='flex flex-col
+                    filter hover:brightness-110 transition duration-150 transform hover:scale-105
+                    cursor-pointer' >
+                     <img 
+                      className='h-10 object-contain '
+                      src={imageToPost} alt='Post Image' />
+                     <p className='text-xs text-red-500 text-center' >Remove</p>
+                   </div>
+              )}
+              
+
             </div>
+
+
+
             <div className='flex justify-evenly p-3 border-t' >
                 <div className='inputIcon'>
                     <VideoCameraIcon className='h-7 text-red-500'/>
                     <p className='text-xs sm:text:sm xl:text-base' >Live Video</p>
                 </div>
 
-                <div className='inputIcon'>
+                <div
+                 onClick={() => filepickerRef.current.click()} 
+                 className='inputIcon'>
                     <EmojiHappyIcon className='h-7 text-yellow-300'/>
                     <p className='text-xs sm:text:sm xl:text-base' >Photo/Video</p>
                     <input 
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import Header from '../components/header/Header';
 import Sidebar from '../components/sidebar/Sidebar';
 import Login from '../components/header/Login';
 import Feed from '../components/feed/Feed';
+import Widgets from '../components/widgets/Widgets';
 import { getSession } from 'next-auth/client';
+import { db } from '../firebase';
 
 
 
-export default function Home({session}) {
+export default function Home({session,posts}) {
   if(!session) {
     return <Login/>
   }
@@ -25,8 +27,8 @@ export default function Home({session}) {
 
            <main className='flex ' >
               <Sidebar/> 
-              <Feed/> 
-             {/* <Widgets/> */}
+              <Feed posts={posts} /> 
+              <Widgets/>
            </main>
       
     </div>
@@ -41,13 +43,24 @@ export const getServerSideProps = async (context) => {
      //get the user 
      const session = await getSession(context);
 
+     const posts = await db.collection('posts').orderBy(
+       'timestamp' , 'desc'
+     ).get();
+
+     const docs = posts.docs.map((post) => ({
+       id:post.id,
+       ...post.data(),
+       timestamp:null
+     }))
+
      return {
        props:{
-         session
+         session,
+         posts:docs
        }
      }
 }
 
 
 
-// min 2.43.30 (state for image upload)
+// min 3.06 (create Post component)
