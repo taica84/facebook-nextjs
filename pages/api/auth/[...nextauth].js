2e61515d8fe0e7248dc36e588d8c0018c6947932
taica84 <taica_rules@yahoo.com>
2021-06-23 17:14:53 +0300

    remove database url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,6 +11,5 @@ export default NextAuth({
     // ...add more providers here
   ],
 
-  // A database is optional, but required to persist accounts in a database
-  database:process.env.DATABASE_URL,
+
 });
